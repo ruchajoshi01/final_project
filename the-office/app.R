a4233efda992ef891a02aab903ea4a8d3b411968
Rucha Joshi <ruchajoshi@college.harvard.edu>
2019-12-03 17:19:03 -0500

    first interactive tab is working again.

--- the-office/app.R
+++ the-office/app.R
@@ -14,128 +14,123 @@ clean_data.df <- read_rds("clean_data.rds")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    #titlePanel("An Analysis of The Office"),
-
+    
     # Navigation bar
-    navbarPage("The Office",
-               
-               # About page
-               tabPanel("About",
-                        
-                        # Header
-                        h1("An Analysis of The Office"),
-                        
-                        
-                        # Image of The Office characters
-                        imageOutput("image", width = "100%", height = "100%"),
-                        
-                        h4("In this app, you will be able to explore The Office. 
-                           The first tab allows you to play around with the number of episodes, scenes, and lines for each character per season.
-                           The second tab shows a sentiment analysis for each character depicting how positive or negative their characters were."),
-                        
-                        h6("Image Source: https://medium.com/@sparks_of_art/ux-research-methods-as-characters-from-the-office-fbcf6d70fd3e")
-               ),
-    
-               # Sidebar with a slider input for number of bins 
-               tabPanel("Per Character",
-               sidebarLayout(
-       
-                sidebarPanel(
-            
-                    # Select season of show
-                    sliderInput("season",
-                        "Season:",
-                        min = 1,
-                        max = 9,
-                        value = 1),
-                    
-                    # Select lines, scenes, words of each character per season
-                    selectInput("char", 
-                                "Characteristic:", 
-                                choices = c("Number of Lines" = "speaker", 
-                                            "Number of Episodes" = "episode", 
-                                            "Number of Scenes" = "scene",
-                                            "Word Frequency" = "freq_word"), 
-                                selected = "speaker")
-                    ),
-
-                # Show a plot of the generated distribution
-                mainPanel(
-           
-                    plotOutput("seasonPlot")
+    navbarPage(
         
-                    )
-    
+        "The Office",
+        
+        # About page
+        tabPanel(
+            
+            "About",
+            
+            # Header
+            h1("An Analysis of The Office"),
+            
+            
+            # Image of The Office characters
+            imageOutput("image", width = "100%", height = "100%"),
+            
+            h4(
+                "In this app, you will be able to explore The Office.
+                
+                The first tab allows you to play around with the number of episodes, scenes, and lines for each character per season.
+                
+                The second tab shows a sentiment analysis for each character depicting how positive or negative their characters were."
+            ),
+            
+            h6(
+                "Image Source: https://medium.com/@sparks_of_art/ux-research-methods-as-characters-from-the-office-fbcf6d70fd3e"
+            )
+            
+        ),
+        
+        # Sidebar with a slider input for number of bins
+        tabPanel("Per Character",
+                 sidebarLayout(
+                     sidebarPanel(
+                         # Select season of show
+                         sliderInput(
+                             "season",
+                             "Season:",
+                             min = 1,
+                             max = 9,
+                             value = 1
+                         ),
+                         
+                         # Select lines, scenes, words of each character per season
+                         selectInput(
+                             "char",
+                             "Characteristic:",
+                             choices = c(
+                                 "Number of Lines" = "speaker",
+                                 "Number of Episodes" = "episode",
+                                 "Number of Scenes" = "scene"),
+                                 selected = "speaker"
+                             )
+                         ),
+                         
+                         # Show a plot of the generated distribution
+                         mainPanel(plotOutput("seasonPlot"))
+                         
+                     )
+                     
+                 ),
+        tabPanel(
+            "Sentiment Analysis",
+            sidebarPanel(
+                # Select a season
+                sliderInput(
+                    "season_senti",
+                    "Season:",
+                    min = 1,
+                    max = 9,
+                    value = 1
+                ),
+                
+                selectInput(
+                    "person",
+                    "Character:",
+                    choices =  c(
+                        "Angela" = "angela",
+                        "Darryl" = "darryl",
+                        "Dwight" = "dwight",
+                        "Jan" = "jan",
+                        "Jim" = "jim",
+                        "Holly" = "holly",
+                        "Kevin" = "kevin",
+                        "Meredith" = "meredith",
+                        "Michael" = "michael",
+                        "Oscar" = "oscar",
+                        "Pam" = "pam",
+                        "Phyllis" = "phyllis",
+                        "Roy" = "roy",
+                        "Ryan" = "ryan",
+                        "Stanley" = "stanley",
+                        "Toby" = "toby"
+                    ),
+                    selected = "angela"
                 )
-
-               ),
-               
-               
-               
-               
-               tabPanel("Sentiment Analysis",
-                        sidebarPanel(
-                            
-                            # Select a season
-                            sliderInput("season_senti",
-                                        "Season:",
-                                        min = 1,
-                                        max = 9,
-                                        value = 1),
-                            
-                            # Select an action
-                            selectInput("action", 
-                                        "Characteristic:", 
-                                        choices =  c("Most Frequent Words" = "freq_words",
-                                                     "Stanley" = "stanley",
-                                                     "Toby" = "toby"), 
-                                        selected = "angela"),
-                            
-                            selectInput("person", 
-                                        "Character:", 
-                                        choices =  c("Angela" = "angela",
-                                                     "Darryl" = "darryl",
-                                                     "Dwight" = "dwight",
-                                                     "Jan" = "jan", 
-                                                     "Jim" = "jim", 
-                                                     "Holly" = "holly",
-                                                     "Kevin" = "kevin",
-                                                     "Meredith" = "meredith",
-                                                     "Michael" = "michael",
-                                                     "Oscar" = "oscar",
-                                                     "Pam" = "pam", 
-                                                     "Phyllis" = "phyllis",
-                                                     "Roy" = "roy",
-                                                     "Ryan" = "ryan",
-                                                     "Stanley" = "stanley",
-                                                     "Toby" = "toby"), 
-                                        selected = "angela")
-                        ),
-                        mainPanel(
-                            
-                            plotOutput("piePlot"),
-                            
-                            dataTableOutput("dataTable")
-                            
-                        )
-                        
-               )
-
+            ),
+            mainPanel(plotOutput("piePlot"),
+                      
+                      dataTableOutput("dataTable"))
+        )
+        
     ))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    
-    
     output$image <- renderImage({
         # Return a list containing the filename and alt text
-        list(src = 'the_office_image.png', 
-             height = 471,
-             width = 628, style="display: block; margin-left: auto; margin-right: auto;")
-    }, deleteFile = FALSE
-    )
+        list(
+            src = 'the_office_image.png',
+            height = 471,
+            width = 628,
+            style = "display: block; margin-left: auto; margin-right: auto;"
+        )
+    }, deleteFile = FALSE)
     
     # Data for first graph of information (episodes, scenes, lines) per character
     datareact1 <- reactive ({
@@ -143,71 +138,92 @@ server <- function(input, output) {
             # Filtering for season
             filter(season == input$season) %>%
             select(season, episode, speaker, scene) %>%
-            filter(speaker %in% c("toby", "stanley", "ryan", "roy", 
-                                "phyllis", "pam", "oscar", "michael", 
-                                "dwight", "meredith", "kevin", "jim", 
-                                "jan", "angela", "darryl"))
+            filter(
+                speaker %in% c(
+                    "toby",
+                    "stanley",
+                    "ryan",
+                    "roy",
+                    "phyllis",
+                    "pam",
+                    "oscar",
+                    "michael",
+                    "dwight",
+                    "meredith",
+                    "kevin",
+                    "jim",
+                    "jan",
+                    "angela",
+                    "darryl"
+                )
+            )
     })
     
     # Determine which dataframe to create
     plotreactive1 <- reactive({
-        if(input$char == "speaker") 
+        if (input$char == "speaker")
         {
             table_speaker <- datareact1()
         }
-        else if(input$char == "scene") {
+        else if (input$char == "scene") {
             table_scene <- datareact1() %>%
-                #group_by(scene, speaker) %>% 
+                #group_by(scene, speaker) %>%
                 count(scene, speaker)
         }
-        else if(input$char == "episode"){
+        else{
             table_episode <- datareact1() %>%
                 count(episode, speaker)
         }
-        else{
-            custom_stop_words <- bind_rows(data_frame(word = c("yeah", "hey", "uh", "um", "huh", "hmm", "ah", "umm", "uhh", "gonna", "na", "ha", "gotta"), 
-                                                      lexicon = c("custom")), 
-                                           stop_words)
-            
-            tidy_tokens <- clean_data %>%
-                select(line = id, line_text_mod, everything(), -line_text, -actions, -deleted) %>% 
-                unnest_tokens(word, line_text_mod, strip_numeric = TRUE) %>%
-                mutate_at(vars(word), funs(str_replace_all(., "'s$", ""))) %>%
-                anti_join(stop_words, by = "word") %>%
-                filter(speaker == input$person) %>%
-                anti_join(custom_stop_words, by = "word") %>%
-                count(speaker, word) %>%
-                arrange(desc(n)) %>%
-                mutate(prop = round((n / sum(n))*100, 1)) %>%
-                head(10)
-        }
     })
-
-    output$seasonPlot <- renderPlot({ #DT::renderDataTable({
+    
+    output$seasonPlot <- renderPlot({
+        #DT::renderDataTable({
         
         plotreactive1() %>%
             ggplot(aes(x = speaker)) +
             geom_bar() +
-            labs(title = "Number of -- spoken by each character in season 1", 
-                 x = "Character", 
+            labs(title = "Number of -- spoken by each character in season 1",
+                 x = "Character",
                  y = "Number of --") +
             coord_flip()
     })
     
     datareact2 <- reactive ({
         tidy_tokens <- clean_data %>%
-            select(line = id, line_text_mod, everything(), -line_text, -actions, -deleted) %>% 
+            select(line = id,
+                   line_text_mod,
+                   everything(),
+                   -line_text,
+                   -actions,
+                   -deleted) %>%
             unnest_tokens(word, line_text_mod, strip_numeric = TRUE) %>%
             mutate_at(vars(word), funs(str_replace_all(., "'s$", ""))) %>%
             anti_join(stop_words, by = "word")
     })
     
     plotreactive2 <- reactive({
-        if(input$action == "freq_words") 
+        if (input$action == "freq_words")
         {
-            custom_stop_words <- bind_rows(data_frame(word = c("yeah", "hey", "uh", "um", "huh", "hmm", "ah", "umm", "uhh", "gonna", "na", "ha", "gotta"), 
-                                                      lexicon = c("custom")), 
-                                           stop_words)
+            custom_stop_words <-
+                bind_rows(data_frame(
+                    word = c(
+                        "yeah",
+                        "hey",
+                        "uh",
+                        "um",
+                        "huh",
+                        "hmm",
+                        "ah",
+                        "umm",
+                        "uhh",
+                        "gonna",
+                        "na",
+                        "ha",
+                        "gotta"
+                    ),
+                    lexicon = c("custom")
+                ),
+                stop_words)
             
             person_freq_words <- datareact2() %>%
                 filter(speaker == input$person) %>%
@@ -215,7 +231,7 @@ server <- function(input, output) {
                 #filter(speaker == input$person) %>%
                 count(speaker, word) %>%
                 arrange(desc(n)) %>%
-                mutate(prop = round((n / sum(n))*100, 1)) %>%
+                mutate(prop = round((n / sum(n)) * 100, 1)) %>%
                 head(10)
         }
         else {
@@ -231,19 +247,19 @@ server <- function(input, output) {
     })
     
     output$charPlot <- renderPlot({
-        
-        plotreactive2 () %>% 
+        plotreactive2 () %>%
             ggplot(aes(x = reorder(word, prop), y = prop)) +
             geom_col() +
-            labs(title = "Frequent Words: [Enter Character Name]", 
-                 x = "", 
+            labs(title = "Frequent Words: [Enter Character Name]",
+                 x = "",
                  y = "Percentage") +
             coord_flip()
     })
     
     output$dataTable <- DT::renderDataTable({
+        
     })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
