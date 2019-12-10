837cbf64e184b5381525c391af06e6b46e21f90e
Rucha Joshi <ruchajoshi@college.harvard.edu>
2019-12-10 12:09:56 -0500

    kinda working? added another tab

--- the-office/app.R
+++ the-office/app.R
@@ -8,43 +8,27 @@
 #
 
 library(shiny)
+library(readr)
+library(tidyverse)
+library(tidytext)
+library(googlesheets4)
+library(RCurl)
+library(SentimentAnalysis)
+library(janitor)
+library(reshape2)
+library(wordcloud)
 
 # Input cleaned data
 clean_data.df <- read_rds("clean_data.rds")
 
-# Define UI for application that draws a histogram
+# Define UI for application
 ui <- fluidPage(
     
     # Navigation bar
     navbarPage(
         
+        # Navigation bar title
         "The Office",
-        
-        # About page
-        tabPanel(
-            
-            "About",
-            
-            # Header
-            h1("An Analysis of The Office"),
-            
-            
-            # Image of The Office characters
-            imageOutput("image", width = "100%", height = "100%"),
-            
-            h4(
-                "In this app, you will be able to explore The Office.
-                
-                The first tab allows you to play around with the number of episodes, scenes, and lines for each character per season.
-                
-                The second tab shows a sentiment analysis for each character depicting how positive or negative their characters were."
-            ),
-            
-            h6(
-                "Image Source: https://medium.com/@sparks_of_art/ux-research-methods-as-characters-from-the-office-fbcf6d70fd3e"
-            )
-            
-        ),
         
         # Page with information about amount of character appearance in the show
         tabPanel("Per Character",
@@ -126,6 +110,107 @@ ui <- fluidPage(
             mainPanel(plotOutput("piePlot"),
                       
                       plotOutput("timePlot"))
+        ),
+        
+        tabPanel(
+            "Relationships",
+            
+            sidebarPanel(
+                
+                selectInput(
+                    "person1",
+                    "Character:",
+                    choices =  c(
+                        "Angela" = "angela",
+                        "Darryl" = "darryl",
+                        "Dwight" = "dwight",
+                        "Jan" = "jan",
+                        "Jim" = "jim",
+                        "Kevin" = "kevin",
+                        "Meredith" = "meredith",
+                        "Michael" = "michael",
+                        "Oscar" = "oscar",
+                        "Pam" = "pam",
+                        "Phyllis" = "phyllis",
+                        "Roy" = "roy",
+                        "Ryan" = "ryan",
+                        "Stanley" = "stanley",
+                        "Toby" = "toby"
+                    ),
+                    selected = "angela"
+                ),
+                
+                selectInput(
+                    "person2",
+                    "Character:",
+                    choices =  c(
+                        "Angela" = "angela",
+                        "Darryl" = "darryl",
+                        "Dwight" = "dwight",
+                        "Jan" = "jan",
+                        "Jim" = "jim",
+                        "Kevin" = "kevin",
+                        "Meredith" = "meredith",
+                        "Michael" = "michael",
+                        "Oscar" = "oscar",
+                        "Pam" = "pam",
+                        "Phyllis" = "phyllis",
+                        "Roy" = "roy",
+                        "Ryan" = "ryan",
+                        "Stanley" = "stanley",
+                        "Toby" = "toby"
+                    ),
+                    selected = "angela"
+                )
+                
+            ),
+            
+            mainPanel(
+                imageOutput("compPlot", width = "100%", height = "100%"),
+                
+                plotlyOutput("relationshipPlot")
+                )
+            
+        ),
+        
+        # About page
+        tabPanel(
+            
+            "About",
+            
+            # Header
+            h1("An Analysis of The Office"),
+            
+            
+            # Image of The Office characters
+            imageOutput("image", width = "100%", height = "100%"),
+            
+            h2(
+                "About this project: "
+            ),
+            
+            h4(
+                "In this app, you will be able to explore The Office.
+                
+                The first tab allows you to play around with the number of episodes, scenes, and lines for each character per season.
+                
+                The second tab shows a sentiment analysis for each character depicting how positive or negative their characters were, 
+                as well as how their sentiments changed over the course of a season."
+            ),
+            
+            h2(
+                "About me: "
+            ),
+            
+            h4(
+                "My name is Rucha Joshi, and I am a sophomore at Harvard College. I am concentrating in Statistics and 
+               planning on doing a secondary in Government. I am from Austin, TX and love the traveling, eating desserts, and snow!" 
+            ),
+            
+            h6(
+                "Image Source: https://medium.com/@sparks_of_art/ux-research-methods-as-characters-from-the-office-fbcf6d70fd3e"
+            )
+            
         )
         
     ))
@@ -138,6 +223,16 @@ server <- function(input, output) {
             src = 'the_office_image.png',
             height = 471,
             width = 628,
+            style = "display: block; margin-left: auto; margin-right: auto;"
+        )
+    }, deleteFile = FALSE)
+    
+    output$compPlot <- renderImage({
+        # Return a list containing the filename and alt text
+        list(
+            src = 'word_freq_corr.png',
+            height = 450,
+            width = 550,
             style = "display: block; margin-left: auto; margin-right: auto;"
         )
     }, deleteFile = FALSE)
@@ -254,6 +349,47 @@ server <- function(input, output) {
                  y = "number",
                  title = paste0("Number of positive and negative words: ", str_to_title(input$person)))
     })
+    
+    # datareactive3 <- reactive () ({
+    #     
+    #     custom_stop_words <- bind_rows(data_frame(word = c("yeah", "hey", "uh", "um", "huh", "hmm", "ah", "umm", "uhh", "gonna", "na", "ha", "gotta"), 
+    #                                               lexicon = c("custom")), 
+    #                                    stop_words)
+    #     
+    #     tidy_tokens <- clean_data %>%
+    #         select(line = id, line_text_mod, everything(), -line_text, -actions, -deleted) %>% 
+    #         unnest_tokens(word, line_text_mod, strip_numeric = TRUE) %>%
+    #         mutate_at(vars(word), funs(str_replace_all(., "'s$", ""))) %>%
+    #         anti_join(custom_stop_words, by = "word") %>%
+    #         filter(speaker %in% c("toby", "stanley", "ryan", "roy", 
+    #                               "phyllis", "pam", "oscar", "michael", 
+    #                               "dwight", "meredith", "kevin", "jim", 
+    #                               "jan", "angela", "darryl")) %>% 
+    #         count(speaker, word, sort = TRUE) %>% 
+    #         group_by(speaker) %>% 
+    #         mutate(proportion = n / sum(n)) %>% 
+    #         select(-n) %>% 
+    #         spread(speaker, proportion) %>%
+    #         select(word, input$person1, input$person2) %>%
+    #         ggplot(aes(x = input$person1, y = input$person2, color = abs(input$person1 - input$person2), label = word)) +
+    #         geom_abline(color = "gray40", lty = 2) +
+    #         geom_jitter(alpha = 0.1, size = 2.5, width = 0.3, height = 0.3) +
+    #         scale_x_log10(labels = percent_format()) +
+    #         scale_y_log10(labels = percent_format()) +
+    #         labs(x = "input$person1",
+    #              y = "input$person2",
+    #              title = "Word Frequncy Comparison: input$person1 and input$person2") +
+    #         theme(legend.position = "none")
+    #     
+    #     ggplotly(tidy_tokens, tooltip = c("word"))
+    #     
+    # })
+    # 
+    # output$relationshipPlot <- renderPlotly({
+    #     datareactive3 ()
+    #         
+    # })
+    
 }
 
 # Run the application
